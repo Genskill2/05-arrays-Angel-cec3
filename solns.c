b857dddea55db3d8e33497d4bf29f474b81b699c
Angel Mary Alex <44723357+Angel-cec3@users.noreply.github.com>
2021-06-18 23:25:32 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -34,3 +34,66 @@ float average(int x[], int c){
 	float avg = sum/(float)c;
 	return avg;
 }
+
+int mode(int x[], int c){
+
+	int l=x[0];
+	int i;
+	for(i=1;i<c;i++){
+		if(x[i]>l)
+			l=x[i];	
+	}
+	int t=l+1;	
+
+	int counts[t];
+	int ctr;
+	for(i=0;i<t;i++){
+		counts[i]=0;
+	}
+
+	for(i=0;i<c;i++){
+
+		counts[x[i]]++;	
+	}
+
+	int k=counts[0];
+	int mode;
+	for(i=1;i<t;i++){
+
+		if(counts[i]>k){
+
+			k=counts[i];
+			mode = i;	
+		}
+	}
+
+	return mode;
+}
+
+int factors(int n, int f[]){
+
+	int i, j, flag;
+	int c = 0;
+	int r=0;
+	while(n%2==0){
+		c++;
+		n=n/2;
+		f[r]=2;
+		r++;
+	}
+	for(i=3; i<=n/2; i=i+2){
+		while(n%i==0){
+			c++;
+			n=n/i;
+			f[r]=i;
+			r++;
+		}
+	}
+	if (n>2){
+		c++;
+		f[r]=n;
+	}
+
+	return c;
+}
+
